b28b9da874eb8b729696dade084302adb654614e
Kenny Catzin Ruiz <kenn2506@gmail.com>
2020-07-25 12:31:51 -0500

    cambios

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 const PROD = 'https://nutri-server.herokuapp.com';
-const LOCAL = 'http://lumenapi.com';
+const LOCAL = 'http://localhost:8080/nutricion-server/public';
 export const URL_SERVICIOS = LOCAL;
